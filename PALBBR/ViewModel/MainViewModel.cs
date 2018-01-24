f34293c862faa7e2c48d3e840314c56d85734c84
SobirASD <reksar_08@mail.ru>
2018-01-24 16:14:55 +0400

    virtual keyboard

--- PALBBR/ViewModel/MainViewModel.cs
+++ PALBBR/ViewModel/MainViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using PALBBR.Data;
@@ -10,37 +11,63 @@ namespace PALBBR.ViewModel
 
     public class MainViewModel : ViewModelBase
     {
-        private int _customerNumber;
+        private string _customerNumber;
+        private ObservableCollection<LinenTest> _lines;
 
-        public int CustomerNumber
+        public string CustomerNumber
         {
             get => _customerNumber;
             set => Set(ref _customerNumber, value);
         }
-        public RelayCommand Add1Command { get; }
+
+        public RelayCommand<object> NumberCommand { get; set; }
+        public RelayCommand<Object> DeleteCommand { get; set; }
+        public RelayCommand PrintCommand { get; set; }
+        public RelayCommand Increase { get; set; }
+
+        public ObservableCollection<LinenTest> Lines
+        {
+            get => _lines;
+            set => Set(ref _lines, value);
+        }
 
         public MainViewModel()
         {
 
-            Add1Command = new RelayCommand(Add1);
+            NumberCommand = new RelayCommand<object>(x => AddNumber(x));
+            DeleteCommand = new RelayCommand<object>(x => DeleteNumber(x));
+            Increase = new RelayCommand(IncreaseQty);
 
-            List<LinenTest> LinenList = new List<LinenTest>();
-            LinenList.Add(new LinenTest() { Name = "Pillow", Qty = 0});
-            LinenList.Add(new LinenTest() { Name = "Bed Sheet", Qty = 4 });
-            LinenList.Add(new LinenTest() { Name = "Shirt", Qty = 0 });
-            LinenList.Add(new LinenTest() { Name = "Trouser", Qty = 6 });
-            LinenList.Add(new LinenTest() { Name = "Towel (small)", Qty = 0 });
-            LinenList.Add(new LinenTest() { Name = "Towel (big)", Qty = 1 });
-            LinenList.Add(new LinenTest() { Name = "Blanket", Qty = 0 });
-            LinenList.Add(new LinenTest() { Name = "Socks (2 pair)", Qty = 0 });
-            LinenList.Add(new LinenTest() { Name = "Underwear (2 Nos)", Qty = 3 });
+            List<LinenTest> items = new List<LinenTest>();
+            items.Add(new LinenTest() { Name = "Pillow", Qty = 0});
+            items.Add(new LinenTest() { Name = "Bed Sheet", Qty = 4 });
+            items.Add(new LinenTest() { Name = "Shirt", Qty = 0 });
+            items.Add(new LinenTest() { Name = "Trouser", Qty = 6 });
+            items.Add(new LinenTest() { Name = "Towel (small)", Qty = 0 });
+            items.Add(new LinenTest() { Name = "Towel (big)", Qty = 1 });
+            items.Add(new LinenTest() { Name = "Blanket", Qty = 0 });
+            items.Add(new LinenTest() { Name = "Socks (2 pair)", Qty = 0 });
+            items.Add(new LinenTest() { Name = "Underwear (2 Nos)", Qty = 3 });
+
+            Lines = new ObservableCollection<LinenTest>(items);
         }
 
-
-        private void Add1()
+        private void IncreaseQty()
         {
-
+            
         }
 
+        private void AddNumber(object x)
+        {
+            if (x == null) return;
+
+            CustomerNumber = $"{CustomerNumber}{x}";
+        }
+
+        private void DeleteNumber(object x)
+        {
+            if (CustomerNumber == null) return;
+            CustomerNumber = null;
+        }
     }
 }
