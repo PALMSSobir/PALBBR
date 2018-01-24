9091fd94c672b781b2cee0c8d7dcf2a613198433
SobirASD <reksar_08@mail.ru>
2018-01-24 08:11:32 +0400

    linen list test

---
+++ PALBBR/Data/LinenTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+
+namespace PALBBR.Data
+{
+    public class LinenTest : ViewModelBase
+    {
+        private string _name;
+        private int _qty;
+
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        public int Qty
+        {
+            get => _qty;
+            set => Set(ref _qty, value);
+        }   
+    }
+}
--- PALBBR/ViewModel/MainViewModel.cs
+++ PALBBR/ViewModel/MainViewModel.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using PALBBR.Data;
 
 
 namespace PALBBR.ViewModel
@@ -6,9 +10,40 @@ namespace PALBBR.ViewModel
 
     public class MainViewModel : ViewModelBase
     {
+        private int _customerNumber;
+
+        public int CustomerNumber
+        {
+            get => _customerNumber;
+            set => Set(ref _customerNumber, value);
+        }
+        public RelayCommand Add1Command { get; }
+
         public MainViewModel()
         {
 
+            Add1Command = new RelayCommand(Add1);
+
+            List<LinenTest> LinenList = new List<LinenTest>();
+            LinenList.Add(new LinenTest() { Name = "Pillow", Qty = 0});
+            LinenList.Add(new LinenTest() { Name = "Bed Sheet", Qty = 4 });
+            LinenList.Add(new LinenTest() { Name = "Shirt", Qty = 0 });
+            LinenList.Add(new LinenTest() { Name = "Trouser", Qty = 6 });
+            LinenList.Add(new LinenTest() { Name = "Towel (small)", Qty = 0 });
+            LinenList.Add(new LinenTest() { Name = "Towel (big)", Qty = 1 });
+            LinenList.Add(new LinenTest() { Name = "Blanket", Qty = 0 });
+            LinenList.Add(new LinenTest() { Name = "Socks (2 pair)", Qty = 0 });
+            LinenList.Add(new LinenTest() { Name = "Underwear (2 Nos)", Qty = 3 });
         }
+
+
+        private void Add1()
+        {
+            if (Qty > 0)
+                Qty = Qty * 10 + 1;
+            else
+                Qty = 1;
+        }
+
     }
 }
