46ca7464afbf7dad7f122928ff5ecbbe40daad99
SobirASD <reksar_08@mail.ru>
2018-01-29 14:44:24 +0400

    Syntaxis Error

---
+++ PALBBR/Data/DeliveryNotes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace PALBBR.Data
+{
+    public class DeliveryNotes : ViewModelBase
+    {
+        private string _id;
+        private string _client;
+        private DateTime _date;
+        private int _deliveryId;
+
+        public string Id
+        {
+            get => _id;
+            set => Set(ref _id, value);
+        }
+        public string Client
+        {
+            get => _client;
+            set => Set(ref _client, value);
+        }
+
+        public DateTime Date
+        {
+            get => _date;
+            set => Set(ref _date, value);
+        }
+        
+        public int DeliveryId
+        {
+            get => _deliveryId;
+            set => Set(ref _deliveryId, value);
+        }   
+    }
+}
--- PALBBR/ViewModel/MainViewModel.cs
+++ PALBBR/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data.OleDb;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using GalaSoft.MvvmLight;
@@ -14,9 +15,10 @@ namespace PALBBR.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        private string _customerNumber;
+        private string _customerNumber = "0";
         private ObservableCollection<LinenList> _linens;
         private LinenList _selectedItem;
+        private DeliveryNotes _deliveryNotes;
         private OleDbConnection Conn;
 
         public string CustomerNumber
@@ -29,11 +31,15 @@ namespace PALBBR.ViewModel
             get => _selectedItem;
             set => Set(ref _selectedItem, value);
         }
-
         public ObservableCollection<LinenList> Linens
         {
             get => _linens;
             set => Set(ref _linens, value);
+        }
+        public DeliveryNotes DeliveryNotes
+        {
+            get => _deliveryNotes;
+            set => Set(ref _deliveryNotes, value);
         }
 
         public RelayCommand<object> NumberCommand { get; set; }
@@ -72,6 +78,7 @@ namespace PALBBR.ViewModel
             Linens = new ObservableCollection<LinenList>(items);
         }
 
+        
         private void PrintBill()
         {
             Conn = new OleDbConnection();
@@ -80,11 +87,28 @@ namespace PALBBR.ViewModel
             Conn.Open();
             OleDbCommand command = new OleDbCommand();
             command.Connection = Conn;
-            string query = "INSERT INTO DeliveryNotes (Client, PrintDate) VALUES('"+ CustomerNumber + "','"+ DateTime.Now+"' )";
+
+            var newId = Guid.NewGuid();
+
+            string query = "INSERT INTO DeliveryNotes (Client, PrintDate, DeliveryID) VALUES('" + CustomerNumber + "','" + DateTime.Now + "', '" + newId + "' )";
             command.CommandText = query;
             MessageBox.Show("Data Saved "+ CustomerNumber);
+            command.ExecuteNonQuery();
+
+            int qty;
+
+            foreach (var item in Linens.Where(x => int.TryParse(x.Qty, out qty) && qty > 0))
+            {
+
+                string query2 = "Insert Into Transaction (ID, LinenId, Quantity) Values('77','88','99')";
+                //string query2 = "Insert Into Transaction (ID, LinenId, Quantity) Values('" + newId + "','" + item.Id + "','" + item.Qty + "')";
+                command.CommandText = query2;
+            }
+            command.ExecuteNonQuery();
+
             Conn.Close();
         }
+
 
         private void AddNumber(object x)
         {
@@ -113,3 +137,43 @@ namespace PALBBR.ViewModel
         }
     }
 }
+// TODO:  так же можно запретить нажимать Print если есть ошибки.Так как я показывал в другом проекте. через ValidationContainer. Но если Вы избавитесь от DevExpress то данный вариант не подойдет
+
+//или можно просто добавить валидацию в методе AddNumber
+
+//если результат будет больше чем макс то ничего не делать
+
+//можно это добавить в CanExecute и тогда кнопки будут блокироваться
+
+//как все заработает, попробуйте работу с БД вынести в отдельный класс RepositoryOleDb, зарегистрировать его в ViewModelLocator
+
+//сответственно он будет реализовывать интерфейс IRepository
+
+//Andrey, 1:51 PM
+//чтение данных из БД:
+
+//если строку
+
+//var item = new LinenList();
+
+//перенести в блок while() { }
+
+//то не нужно будет создавать еще один объект при добавлении в коллекцию
+
+//видимо вы так пофиксили проблему, что в каждой записи были одни и те же значения.
+
+//вы добавляли один и тот же объект в коллекцию и при этом при чтении из бд меняли значения свойств, в результате у вас в списке был один элемент продублированный n раз со значениями свойств из последней записи в БД
+
+//работу с БД нужно обернуть в блок try catch finaly
+
+//а finaly закрывать коннектиан
+
+//так же можно посмотреть как тут делают
+
+//https://stackoverflow.com/questions/12081111/getting-values-back-from-oledbdatareader-reading-from-access-database
+
+//Andrey, 1:56 PM
+//using гарантирует, что будет вызван метод Dispose, в вашем случае он не вызывается и могут быть утечки памяти, приложение будет кушать все больше и больше
+
+//using нужен для reader и для connection.для connection лучше try catch finaly, using тогда не нужен, в блоке finaly сделать conn.Close()
+    //
